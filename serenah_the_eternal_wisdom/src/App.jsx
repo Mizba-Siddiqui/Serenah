16cac06df4d5942bdef7d9aea620de41b2207580
Mizba Siddiqui <mizbasiddiqui217@gmail.com>
2024-11-14 14:48:39 +0530

    Serenah first commit

---
+++ serenah_the_eternal_wisdom/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold font-">
+       Serenah
+      </h1>
+  
+      <div class="font-sans text-gray-800 text-lg leading-relaxed">
+        Welcome to Serenah: The Eternal Wisdom
+      </div>
+
+    
+      <div class="font-amiri text-lg text-gray-700 leading-relaxed">
+        Welcome to Serenah: The Eternal Wisdom
+      </div>
+      <h1 class="font-amiri text-4xl text-center text-gray-900 mt-8">
+        Serenah: The Eternal Wisdom
+      </h1>
+      <p class="font-sans text-lg text-gray-700">
+        Discover exclusive Islamic essentials and unique gifts at Serenah, a place where wisdom meets elegance.
+      </p>
+  
+    </>
+  )
+}
+
+export default App
