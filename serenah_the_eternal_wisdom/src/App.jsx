ec5b3b8f803c89cdf95d97233977d5edddec75a6
Mizba Siddiqui <mizbasiddiqui217@gmail.com>
2024-11-16 04:07:49 +0530

    Footer included

--- serenah_the_eternal_wisdom/src/App.jsx
+++ serenah_the_eternal_wisdom/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 
@@ -9,25 +7,52 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold font-">
-       Serenah
-      </h1>
-  
-      <div class="font-sans text-gray-800 text-lg leading-relaxed">
+      <div className="flex items-center space-x-2 mt-10">
+        <h1 className="text-3xl font-bold">Serenah</h1>
+        <img
+          src="https://github.com/Mizba-Siddiqui/Serenah/blob/main/serenah_the_eternal_wisdom/src/images/WhatsApp%20Image%202024-11-14%20at%2005.03.54_cbac0c6c.png?raw=true"
+          style={{
+            width: 70,
+            height: 70,
+          }}
+          alt="Serenah logo"
+        />
+      </div>
+      <div className="font-sans text-gray-800 text-lg leading-relaxed">
         Welcome to Serenah: The Eternal Wisdom
       </div>
 
     
-      <div class="font-amiri text-lg text-gray-700 leading-relaxed">
+      <div className="font-amiri text-lg text-gray-700 leading-relaxed">
         Welcome to Serenah: The Eternal Wisdom
       </div>
-      <h1 class="font-amiri text-4xl text-center text-gray-900 mt-8">
-        Serenah: The Eternal Wisdom
+      
+      <div className="flex items-center space-x-2 justify-center ">
+        <img
+          src="src/images/WhatsApp%20Image%202024-11-14%20at%2005.03.54_cbac0c6c.png?raw=true"
+          style={{
+            width: 50,
+            height: 50,
+          }}
+          alt="Serenah logo"  />
+      <h1 className="font-amiri text-4xl text-center text-gray-900  ">
+       Serenah: The Eternal Wisdom
       </h1>
-      <p class="font-sans text-lg text-gray-700">
+        
+        <img
+          src="src/images/Screenshot 2024-11-14 170025.png"
+          style={{
+            // width: 60,
+            height: 100,
+          }}
+          alt="Serenah logo" />
+      
+      </div>     
+
+
+      <p className="font-sans text-lg text-gray-700 text-center">
         Discover exclusive Islamic essentials and unique gifts at Serenah, a place where wisdom meets elegance.
       </p>
-  
     </>
   )
 }
